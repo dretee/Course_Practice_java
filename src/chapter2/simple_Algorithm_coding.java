144aac602d135cc6b5bb809b6a032f5d9b0483fb
Tony <uyahanthony008@gmail.com>
2023-10-04 08:20:33 +0100

    initial commit

--- src/chapter2/simple_Algorithm_coding.java
+++ src/chapter2/simple_Algorithm_coding.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class simple_Algorithm_coding {
     public static void main(String[] arg){
 
-        // get the name of a season of the year form the user
+        // get the name of a season of the year form the users
         System.out.print("Please what season of the year would you like to choose? ");
         Scanner scan = new Scanner(System.in);
         String season = scan.nextLine();
@@ -15,7 +15,6 @@ public class simple_Algorithm_coding {
         // get the adjective describing the season
         System.out.print("Please what adjective would describe the season? ");
         String adjective = scan.next();
-
         // use all the input to write a sentence
         System.out.print("On a "+ adjective +" "+ season+ " i would normally take in "+ numbers + " cups of tea");
     }
